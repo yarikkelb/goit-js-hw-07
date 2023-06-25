b44072c79038b3bb67822c00a47018e521052db1
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-25 14:05:04 +0300

    1404

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryElements = document.querySelector('.gallery');
+
+
+// Формуємо розмітку галереї на основі масиву даних
+const createGalleryMarkup = galleryItems => {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    })
+    .join('');
+};
+
+// Рендеримо розмітку галереї
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.5,
+    captionDelay: 250,
+    captionsData: 'alt',
+    fadeSpeed: 250,
+    rtl: true,
+});
