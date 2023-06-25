cbe0a92ee1a556c220e271ed1ee27c9b20ff948a
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-25 19:50:02 +0300

    2506

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,81 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
 
-
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const galleryElements = document.querySelector('.gallery');
-
-let modal; // оголошуємо змінну modal в глобальному контексті
-
-// Формуємо розмітку галереї на основі масиву даних
-const createGalleryMarkup = galleryItems => {
-    return galleryItems
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
     .map(({ preview, original, description }) => {
-        return `
-        <div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-            />
-        </a>
-        </div>
-        `;
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
     .join('');
-};
-
-// Рендеримо розмітку галереї
-galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
-
-// Відкриваємо модальне вікно з великим зображенням
-galleryElements.addEventListener('click', event => {
-    event.preventDefault(); // Забороняємо стандартну дію по кліку на посиланні
-
-    // Перевіряємо, що клікнуто на зображенні
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-
-    // Отримуємо url великого зображення з data-атрибута
-    const originalImageUrl = event.target.dataset.source;
-
-    // Відкриваємо модальне вікно з великим зображенням
-    modal = basicLightbox.create(`
-        <div>
-            <img src="${originalImageUrl}" width="832" height="554" />
-        </div>
-    `);
-
-    modal.show();
-
-  document.addEventListener('keydown', escapeListener);
-  
-  // Закриваємо модальне вікно при кліку на велике зображення
-modal.element().addEventListener('click', event => {
-  if (event.target.nodeName === 'IMG') {
-    modal.close();
-  }
-});
-
-});
-
-// Закриваємо модаль після натискання клавіші Escape
-function escapeListener(event) {
-  if (event.code === 'Escape' && modal) {
-    modal.close();
-    document.removeEventListener('keydown', escapeListener);
-  }
-}
-
-// Закриваємо модальне натискання за межами модального вмісту
-if (modal) {
-  modal.on('close', () => {
-    document.removeEventListener('keydown', escapeListener);
-  });
 }
