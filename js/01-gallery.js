fa3db2c3ed35083df18606092a248cb0ee0e4c4e
yarikkelb <125504558+yarikkelb@users.noreply.github.com>
2023-06-26 20:12:24 +0300

    2012

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,63 @@
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryElements = document.querySelector('.gallery');
 
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-galleryContainer.addEventListener('click', onImgClick);
+let modal; 
 
-// rendered items
-function createGalleryItemsMarkup(items) {
-  return items
+const createGalleryMarkup = galleryItems => {
+    return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+            />
+        </a>
+        </div>
+        `;
     })
     .join('');
+};
+
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+galleryElements.addEventListener('click', event => {
+    event.preventDefault(); // Забороняємо стандартну дію по кліку на посиланні
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const originalImageUrl = event.target.dataset.source;
+
+    modal = basicLightbox.create(`
+        <div>
+            <img src="${originalImageUrl}" width="832" height="554" />
+        </div>
+    `);
+
+    modal.show();
+  document.addEventListener('keydown', escapeListener);
+modal.element().addEventListener('click', event => {
+  if (event.target.nodeName === 'IMG') {
+    modal.close();
+  }
+});
+});
+
+function escapeListener(event) {
+  if (event.code === 'Escape' && modal) {
+    modal.close();
+    document.removeEventListener('keydown', escapeListener);
+  }
+}
+
+if (modal) {
+  modal.on('close', () => {
+    document.removeEventListener('keydown', escapeListener);
+  });
 }
